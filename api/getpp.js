38251468eb7af8947df7c6920bce5cc17c6012d0
ause97818-arch <ause97818@gmail.com>
2026-07-31 08:24:23 +0530

    Update getpp.js

--- api/getpp.js
+++ api/getpp.js
@@ -1,32 +1,53 @@
-// api/getpp.js
-const {
-  setCorsHeaders,
-  handlePreflight,
-  resolveAlias,
-  fetchWithRetry,
-  sendUpstreamError
-} = require('../lib/apiHandler');
+import axios from "axios";
 
-const TARGET_BASE_URL = 'http://45.13.226.96:9024/api/getpp';
+const TARGET_BASE_URL = "http://45.13.226.96:9024/api/getpp";
+const REQUEST_TIMEOUT_MS = 8000;
 
-module.exports = async (req, res) => {
-  if (handlePreflight(req, res)) return;
+function setCorsHeaders(res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+}
+
+export default async function handler(req, res) {
   setCorsHeaders(res);
 
-  try {
-    const num = resolveAlias(req.query, 'num', 'number');
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 
-    if (!num) {
+  try {
+    const { num, number } = req.query;
+    const phoneNumber = num || number;
+
+    if (!phoneNumber) {
       return res.status(400).json({
         success: false,
-        error: 'Missing required parameter: "num" is required.'
+        error: "Missing required parameter: num (or number)",
       });
     }
 
-    const upstreamResponse = await fetchWithRetry(TARGET_BASE_URL, { num });
-    return res.status(200).json(upstreamResponse.data);
+    const targetUrl = `${TARGET_BASE_URL}?num=${encodeURIComponent(phoneNumber)}`;
 
+    try {
+      const remoteResponse = await axios.get(targetUrl, {
+        timeout: REQUEST_TIMEOUT_MS,
+      });
+
+      return res.status(200).json(remoteResponse.data);
+    } catch (fetchError) {
+      return res.status(502).json({
+        success: false,
+        error: "Upstream server unreachable or timed out",
+        details: fetchError.message,
+      });
+    }
   } catch (error) {
-    return sendUpstreamError(res, error);
+    setCorsHeaders(res);
+    return res.status(500).json({
+      success: false,
+      error: "Internal Server Error",
+      details: error.message,
+    });
   }
-};
+}
